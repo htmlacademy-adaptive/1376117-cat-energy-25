aa6f78d3aec6ab25ff12b7161bcd8652d4f632cc
AnnaPikaleva <97855987+AnnaPikaleva@users.noreply.github.com>
2022-04-29 17:12:42 +0300

    Декоративные элементы главной страницы

---
+++ source/js/script.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--close')) {
+    navMain.classList.remove('main-nav--close');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--close');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
